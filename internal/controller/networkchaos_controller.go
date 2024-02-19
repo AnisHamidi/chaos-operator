d14300ac0c68114413bc369231d7023ce438bccb
Anis <anis@Aniss-MacBook-Air.local>
2024-02-19 16:29:57 +0330

    test

--- internal/controller/networkchaos_controller.go
+++ internal/controller/networkchaos_controller.go
@@ -401,7 +401,7 @@ func (r *NetworkChaosReconciler) manageToxics(ctx context.Context, req ctrl.Requ
 	//	}
 	// if networkChaos.Spec.TimeoutToxic.Timeout > 0 {
 	_, err = proxy.AddToxic(networkChaos.GetName()+"-timeout", "timeout", networkChaos.Spec.Stream, networkChaos.Spec.TimeoutToxic.Probability, toxiproxy.Attributes{
-		"latency":  networkChaos.Spec.TimeoutToxic.Timeout,
+		"timeout":  networkChaos.Spec.TimeoutToxic.Timeout,
 		"toxicity": networkChaos.Spec.TimeoutToxic.Probability,
 	})
 	if err != nil {
