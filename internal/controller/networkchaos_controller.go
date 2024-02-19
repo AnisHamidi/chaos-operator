230ed54eb14469b63411e7e3f4e2a58eae6ce2ce
Anis <anis@Aniss-MacBook-Air.local>
2024-02-19 15:36:08 +0330

    test

--- internal/controller/networkchaos_controller.go
+++ internal/controller/networkchaos_controller.go
@@ -401,7 +401,8 @@ func (r *NetworkChaosReconciler) manageToxics(ctx context.Context, req ctrl.Requ
 	//	}
 	// if networkChaos.Spec.TimeoutToxic.Timeout > 0 {
 	_, err = proxy.AddToxic("test-timeout", "timeout", networkChaos.Spec.Stream, networkChaos.Spec.TimeoutToxic.Probability, toxiproxy.Attributes{
-		"timeout": 3000,
+		"timeout":  networkChaos.Spec.TimeoutToxic.Timeout,
+		"toxicity": networkChaos.Spec.TimeoutToxic.Probability,
 	})
 	if err != nil {
 		log.Error(err, "Failed to create timeout toxic")
